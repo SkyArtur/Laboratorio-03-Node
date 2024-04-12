6bec6a5d46c55a069d8965dfb4ff7f14a7e42476
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-04-11 22:52:34 -0300

    Criando uma conexão com o banco de dados e realizando Testes.

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const database1 = require('./connector').connect()
+const Connector = require('./connector').connect()
 
 
-database1.setQuery('SELECT * FROM selecionar_produto_em_estoque($1);', 'abacate')
-database1.execute()
-    .then(console.log)
-    .catch(console.error)
+module.exports = { Connector }
